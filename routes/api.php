4afd5041c8e18b286f06aefdcf90d713da02233c
Pongv <pongvarid@gmail.com>
2018-07-04 08:50:24 +0700

    Change Web to Api

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,20 @@ Route::middleware(['auth:api'])->group(function () {
 
     Route::resource('/farm/pigs', Farm\PigController::class);
     Route::resource('/farm/pigs.cycles', Farm\PigCycleController::class);
+    Route::get('/pigcycle',Farm\PigCycleController::class.'@getCycleID');
+
+    Route::get('/breederData',Farm\PigCycleController::class.'@getBreederData');
+    Route::get('/birthData',Farm\PigCycleController::class.'@getBirthData');
+    Route::get('/milkData',Farm\PigCycleController::class.'@getMilkData');
+    Route::get('/getVaccineData',Farm\PigCycleController::class.'@getVaccineData');
+
+    Route::post('/save.breeder', Farm\PigCycleController::class. '@storeBreeder');
+    Route::post('/save.birth', Farm\PigCycleController::class. '@storeBirth');
+    Route::post('/save.milk', Farm\PigCycleController::class. '@storeMilk');
+    Route::post('/save.vaccine', Farm\PigCycleController::class. '@storeVaccine');
+
+
+    Route::get('/choice',Farm\PigCycleController::class.'@getChoice');
+    Route::get('/vaccine',Farm\PigCycleController::class.'@getVaccineRecent');
 
 });
-
