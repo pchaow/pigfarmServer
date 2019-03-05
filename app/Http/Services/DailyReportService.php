c5f13c6487a0f9c60bbcd21061f56d7eb48ce63c
Chaow Porkaew <chaow.po@up.ac.th>
2019-03-05 17:43:03 +0700

    add stub daily report service

--- app/Http/Services/DailyReportService.php
+++ app/Http/Services/DailyReportService.php
@@ -8,11 +8,24 @@
 
 namespace App\Http\Services;
 
+use App\Models\Pig;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class DailyReportService extends BaseService
 {
+    function generateDailyReport($currentDate){
 
+        $query = Pig::query();
+        $query->where('status','=','PIGSTATUS_001');
+        $countPigId = $query->count('id');
+
+        return [
+            'report_date' => $currentDate,
+            'active_breeder' => $countPigId,
+        ];
+
+
+    }
 }
