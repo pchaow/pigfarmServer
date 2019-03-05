745d12d1e6a8b1792eff4b177e90b85d81311cbe
Chaow Porkaew <chaow.po@up.ac.th>
2019-03-05 17:24:43 +0700

    add daily report service

---
+++ app/Http/Services/DailyReportService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chaow
+ * Date: 3/29/2018
+ * Time: 10:04 AM
+ */
+
+namespace App\Http\Services;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class DailyReportService extends BaseService
+{
+
+}
