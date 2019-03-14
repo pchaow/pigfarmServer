d667dd4fb354af880dc5a50ad3916bbb2ecc468f
Chaow Porkaew <chaow.po@up.ac.th>
2019-03-14 22:52:22 +0700

    change cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,23 +1,19 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Closure;
+class Cors {
 
-class Cors
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request)
-           ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+    public function handle($request, Closure $next) {
+        $allowedOrigins = ['*'];
+        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        if (in_array($origin, $allowedOrigins)) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $origin)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With, cache-control,postman-token, token')
+                ->header('Access-Control-Allow-Credentials',' true');
+        }
+        return $next($request);
     }
 }
