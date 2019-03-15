7210baa82f5610f2357f1c0154911b1338a95608
pongvarid <pongvarid@gmail.com>
2019-03-16 04:24:15 +0700

    cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace App\Http\Middleware;
-use Closure;
 class Cors {
-
     public function handle($request, Closure $next) {
+        $allowedOrigins = ['*'];
+        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+        if (in_array($origin, $allowedOrigins)) {
             return $next($request)
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', '*')
                 ->header('Access-Control-Allow-Headers','*')
                 ->header('Access-Control-Allow-Credentials',' true');
+        }
+        return $next($request);
     }
 }
