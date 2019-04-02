6a87b9add23d32a7f45a4608fc3689c411782b78
Chaow Porkaew <chaow.po@up.ac.th>
2019-04-02 12:39:32 +0700

    update cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,8 +44,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\Cors::class, //added here
-
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -65,6 +64,5 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
